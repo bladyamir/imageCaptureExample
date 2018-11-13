9c4b3c2408ee8f7781fa6827fa488a4c8ce564d6
Amir <mr.amir.1971@gmail.com>
2018-11-13 16:44:00 +0600

    recyclerView preview and delete option added

--- app/src/main/java/com/example/amir/recyclerviewitemclose/CustomAdapter.java
+++ app/src/main/java/com/example/amir/recyclerviewitemclose/CustomAdapter.java
@@ -1,5 +1,6 @@
 package com.example.amir.recyclerviewitemclose;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -15,30 +16,39 @@ import java.util.List;
 public class CustomAdapter extends
         RecyclerView.Adapter<CustomAdapter.ViewHolder>
 {
-    private List<Uri> images;
+    private List<Bitmap> images;
+
+
+    public CustomAdapter(List<Bitmap> images) {
+        this.images = images;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i)
     {
-        View
-
-                view= LayoutInflater.from(parent.getContext())
+        View view= LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.row_item,parent,false);
         return new ViewHolder(view);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int i)
     {
         Glide.with(holder.itemView.getContext())
-                .load(images.get(i).getPath())
+                .load(images.get(i))
                 .into(holder.imgContent);
+        final int position=i;
 
         holder.imgClose.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
+                images.remove(position);
+
+                notifyDataSetChanged();
 
             }
         });
@@ -59,5 +69,12 @@ public class CustomAdapter extends
             imgClose=itemView.findViewById(R.id.img_close);
             imgContent=itemView.findViewById(R.id.img_content);
         }
+    }
+
+    public void update(Bitmap image){
+
+        images.add(image);
+
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/example/amir/recyclerviewitemclose/MainActivity.java
+++ app/src/main/java/com/example/amir/recyclerviewitemclose/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.amir.recyclerviewitemclose;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Bitmap> imageList=new ArrayList<>();
+
+    private   CustomAdapter adapter;
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +33,33 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.acivity_main);
 
+        recyclerView=findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        adapter=new CustomAdapter(imageList);
+
+        recyclerView.setAdapter(adapter);
+
         Button button=findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 if(isPermissionGranted()){
-                    
+
+                    Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    if(isAnyCameraAppInstalled(intent)){
+
+                        startActivityForResult(intent,2);
+                    }
                 }else {
                     if(isUserCheckedNeverAskAgain()){
+
                         askPermission();
+
                     }else {
 
                         askPermission();
@@ -39,17 +71,45 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==2 && resultCode==RESULT_OK){
+
+            Bitmap image=getTumbNail(data);
+
+            adapter.update(image);
+        }
+    }
+
     private void askPermission() {
 
-        ActivityCompat.requestPermissions(this,n);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
 
     }
 
     public boolean isPermissionGranted() {
+
         return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED;
     }
 
     public boolean isUserCheckedNeverAskAgain() {
+
         return !ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    }
+
+    public boolean isAnyCameraAppInstalled(Intent intent) {
+
+        return intent.resolveActivity(getPackageManager())!=null;
+    }
+
+    public Bitmap getTumbNail(Intent data) {
+
+        Bundle bundle=data.getExtras();
+
+        Bitmap bitmap= (Bitmap) bundle.get("data");
+
+        return bitmap;
     }
 }
